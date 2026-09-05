661f51e9e3a80d03889d874e2b92315c7d57f884
Khalid <khalidwaleed.khalid@itsc.de>
2026-09-05 15:17:12 +0200

    Set correct base path for GitHub Pages (repo: notes_app)

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES ? '/noten-app/' : '/',
+  base: process.env.GITHUB_PAGES ? '/notes_app/' : '/',
   server: { host: true, port: 5173 },
   build: { outDir: 'dist' },
 })
